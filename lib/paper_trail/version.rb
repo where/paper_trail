87aff16329c91ed746b4ebd89e99324c2946b025
Ken Mazaika <kenmazaika@gmail.com>
2013-03-27 16:53:56 -0400

    rails4 doesn't like scoped.

--- lib/paper_trail/version.rb
+++ lib/paper_trail/version.rb
@@ -4,7 +4,7 @@ class Version < ActiveRecord::Base
   attr_accessible :item_type, :item_id, :event, :whodunnit, :object, :object_changes
 
   def self.with_item_keys(item_type, item_id)
-    scoped(:conditions => { :item_type => item_type, :item_id => item_id })
+    where(:item_type => item_type, :item_id => item_id)
   end
 
   def self.creates
